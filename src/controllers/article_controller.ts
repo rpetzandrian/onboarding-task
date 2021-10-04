80c97909d36c1d45dcf01a75b75a890eb30cc78d
rpetzandrian <rpetzandrian@gmail.com>
2021-10-04 10:45:51 +0700

    :rpetzandrian implement dto & validation

--- src/controllers/article_controller.ts
+++ src/controllers/article_controller.ts
@@ -1,6 +1,8 @@
 import { Context, Controller, RequestData } from 'rey-common';
+import { ArticleParamsDTO, ArticleRequestDTO } from 'src/entity/dto/article';
 import { API_ROUTE } from '../entity/constant/api';
 import ArticleService from '../services/article_service';
+import { SCHEME, COMMON_PARAMS_ID } from '../entity/validation/common';
 
 export default class ArticleController extends Controller {
     constructor(
@@ -13,28 +15,28 @@ export default class ArticleController extends Controller {
         return await this.ArticleService.getArticles();
     }
 
-    public async getArticleById(data: RequestData, context: Context): Promise<any> {
+    public async getArticleById(data: RequestData<ArticleParamsDTO>, context: Context): Promise<any> {
         return await this.ArticleService.getArticleById(data.params.id);
     }
 
-    public async createArticle(data: RequestData, context: Context): Promise<any> {
+    public async createArticle(data: RequestData<Partial<ArticleRequestDTO>>, context: Context): Promise<any> {
         return await this.ArticleService.createArticle(data.body);
     }
 
-    public async updateArticle(data: RequestData, context: Context): Promise<any> {
+    public async updateArticle(data: RequestData<Partial<ArticleRequestDTO>>, context: Context): Promise<any> {
         return await this.ArticleService.updateArticle(data.body);
     }
 
-    public async deleteArticle(data: RequestData, context: Context): Promise<any> {
+    public async deleteArticle(data: RequestData<ArticleParamsDTO>, context: Context): Promise<any> {
         return await this.ArticleService.deleteArticle(data.params.id);
     }
 
     public setRoutes(): void {
         /** router level caching */
         this.addRoute('get', '/', this.getArticle.bind(this), { cache: false });
-        this.addRoute('get', '/:id', this.getArticleById.bind(this), { cache: false });
-        this.addRoute('post', '/', this.createArticle.bind(this), { cache: false });
-        this.addRoute('put', '/', this.updateArticle.bind(this), { cache: false });
-        this.addRoute('delete', '/:id', this.deleteArticle.bind(this), { cache: false });
+        this.addRoute('get', '/:id', this.getArticleById.bind(this), { cache: false, validate: COMMON_PARAMS_ID });
+        this.addRoute('post', '/', this.createArticle.bind(this), { cache: false, validate: SCHEME.CREATE_ARTICLE });
+        this.addRoute('put', '/', this.updateArticle.bind(this), { cache: false, validate: SCHEME.UPDATE_ARTICLE });
+        this.addRoute('delete', '/:id', this.deleteArticle.bind(this), { cache: false, validate: COMMON_PARAMS_ID });
     }
 }
--- src/controllers/author_controller.ts
+++ src/controllers/author_controller.ts
@@ -1,4 +1,6 @@
 import { Context, Controller, RequestData } from 'rey-common';
+import { AuthorParamsDTO, AuthorRequestDTO } from '../entity/dto/author';
+import { COMMON_PARAMS_ID, SCHEME } from '../entity/validation/common';
 import { API_ROUTE } from '../entity/constant/api';
 import AuthorService from '../services/author_service';
 
@@ -13,28 +15,28 @@ export default class AuthorController extends Controller {
         return await this.AuthorService.getAuthors();
     }
 
-    public async getAuthorById(data: RequestData, context: Context): Promise<any> {
+    public async getAuthorById(data: RequestData<AuthorParamsDTO>, context: Context): Promise<any> {
         return await this.AuthorService.getAuthorById(data.params.id);
     }
 
-    public async createAuthor(data: RequestData, context: Context): Promise<any> {
+    public async createAuthor(data: RequestData<Partial<AuthorRequestDTO>>, context: Context): Promise<any> {
         return await this.AuthorService.createAuthor(data.body);
     }
 
-    public async updateAuthor(data: RequestData, context: Context): Promise<any> {
+    public async updateAuthor(data: RequestData<Partial<AuthorRequestDTO>>, context: Context): Promise<any> {
         return await this.AuthorService.updateAuthor(data.body);
     }
 
-    public async deleteAuthor(data: RequestData, context: Context): Promise<any> {
+    public async deleteAuthor(data: RequestData<AuthorParamsDTO>, context: Context): Promise<any> {
         return await this.AuthorService.deleteAuthor(data.params.id);
     }
 
     public setRoutes(): void {
         /** router level caching */
         this.addRoute('get', '/', this.getAuthors.bind(this), { cache: false });
-        this.addRoute('get', '/:id', this.getAuthorById.bind(this), { cache: false });
-        this.addRoute('post', '/', this.createAuthor.bind(this), { cache: false });
-        this.addRoute('put', '/', this.updateAuthor.bind(this), { cache: false });
-        this.addRoute('delete', '/:id', this.deleteAuthor.bind(this), { cache: false });
+        this.addRoute('get', '/:id', this.getAuthorById.bind(this), { cache: false, validate: COMMON_PARAMS_ID });
+        this.addRoute('post', '/', this.createAuthor.bind(this), { cache: false, validate: SCHEME.CREATE_AUTHOR });
+        this.addRoute('put', '/', this.updateAuthor.bind(this), { cache: false, validate: SCHEME.UPDATE_AUTHOR });
+        this.addRoute('delete', '/:id', this.deleteAuthor.bind(this), { cache: false, validate: COMMON_PARAMS_ID });
     }
 }
